3580d5d1855efc452b9c130011d362c82267613a
Kroos Xiang <kroosvi@gmail.com>
2024-03-24 12:27:58 +0800

    Changes made but not much

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-impl algorithms{
+struct Algorithms;
+impl Algorithms{
+    pub fn new() -> Self{
+        Self
+    }
+
     //---------------------------Heap---------------------//
     //Note: Tree base heap are more memory used than the array based
     //This I would try to use build both in rust
 
     pub fn min_heap(original_vec:&mut [u64]){
         //heapify
-        heapify();
+        Self::heapify(original_vec);
     }
 
     fn swap(original_vec:&mut [u64], firstNode : u32){
@@ -14,18 +19,15 @@ impl algorithms{
 
     fn heapify(original_vec:&mut [u64]){
         let mut starting_node = original_vec.len()/2;
-        starting_node = starting_node.ceil() - 1;
-
+        starting_node = starting_node.div_ceil(0) - 1;
     }
     //----------------------Heap--------------------------//
 }
 
 fn main() {
-    let mut array = vec![2,3,5,6,8,9];
+    let mut array : Vec<u64> = vec![2,3,5,6,8,9];
 
-    let algorithm = algorithms::new();
-
-    algorithm.min_heap(&array);
+    let algorithm = Algorithms::new();
 }
 
 
